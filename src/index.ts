4511d28357dd4054d3fdc6927c779a6054685530
Piyush Rajendra <piyush.rajendra@gmail.com>
2024-06-14 14:29:35 -0400

    minor changes

--- src/index.ts
+++ src/index.ts
@@ -13,16 +13,6 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Hello from Express and TypeScript');
 });
 
-// Example route to fetch data from the database
-app.get('/users', (req: Request, res: Response) => {
-  connection.query('SELECT * FROM users', (err, results) => {
-    if (err) {
-      res.status(500).send(err);
-      return;
-    }
-    res.json(results);
-  });
-});
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
