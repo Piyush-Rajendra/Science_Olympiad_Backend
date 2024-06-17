0d594a4d19590aaeec0cc6dc45fe39a5f10979fd
Piyush Rajendra <piyush.rajendra@gmail.com>
2024-06-17 03:33:29 -0400

    commmited

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,19 @@
 import express, { Request, Response } from 'express';
 import connection from '../config/db.config';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 dotenv.config();
 
 const app = express();
 const port = 3001;
+const setup =  connection.query
+app.use(cors());
 
 app.use(express.json());
 
 app.get('/', (req: Request, res: Response) => {
+  setup;
   res.send('Hello Backend is wokrking');
 });
 
