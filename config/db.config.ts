f3bf2e983b4d87305286dd9fb6305ab4eb997047
Piyush Rajendra <piyush.rajendra@gmail.com>
2024-07-05 18:59:14 -0400

    db setup

--- config/db.config.ts
+++ config/db.config.ts
@@ -9,14 +9,20 @@ const pool: Pool = createPool({
 });
 
 pool.getConnection()
-  .then(connection => {
-    console.log('Database connection established');
-    connection.release();
-  })
-  .catch(error => {
-    console.error('Error connecting to database:', error.message);
-    process.exit(1); // Exit the process or handle the error as per your application's needs
-  });
+.then(async connection => {
+  console.log('Database connection established');
 
-  
+  // Check if the database exists, create it if it doesn't
+  await connection.query(`CREATE DATABASE IF NOT EXISTS science_olympiad`);
+
+  connection.release();
+})
+.then(() => {
+  console.log('Database setup complete');
+})
+.catch(error => {
+  console.error('Error connecting to database:', error.message);
+  process.exit(1); // Exit the process or handle the error as per your application's needs
+});
+
 export default pool;
